8cc932cc712ba954388873251914454acff58ef9
suim <spdlqjtn159@gmail.com>
2022-04-20 11:43:45 +0900

    js checkbox 체크 여부

--- js/sub1.js
+++ js/sub1.js
@@ -1,10 +1,25 @@
-const nameValue = document.getElementById('name');
-const checkBox = document.getElementById('check-box');
+// 상담
+let nameValue = document.getElementById('name');
+let checkBox = document.getElementById('check-box');
+let selectBox = document.getElementById('select-box');
+let is_check = false;
 
 console.dir(nameValue);
 console.log(checkBox);
+console.dir(selectBox);
+console.log(is_check);
 
 checkBox.addEventListener('click', event =>{
-
+  
+  if(selectBox.checked == true){
+    alert(nameValue.value + '님의 신청을 접수하였습니다.');
+    is_check = true;
+  }
+  else if(selectBox.checked == false){
+    alert('동의');
+    is_check =false;
+  }
   nameValue.value ="";
 })
+
+// 자리
