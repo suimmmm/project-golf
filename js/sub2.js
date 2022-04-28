34935107fe99ef7be9d307016bee86d02492e52a
suim <spdlqjtn159@gmail.com>
2022-04-28 16:37:11 +0900

    프로필 사진 업로드 js 작업중

--- js/sub2.js
+++ js/sub2.js
@@ -1,33 +1,28 @@
 // 이미지 불러오기
 const changeImg = document.getElementById('change-img');
-const inputFile = document.getElementById('input-file');
-const imgBox = document.getElementById('uploadimage');
-const checkBox = document.getElementById('check-box');
+const imgBox = document.getElementById('img-box');
 const fileBox = document.getElementById('file-box');
+const fileInput = document.querySelector('input').files[0];
 let fileBoxVisible = window.getComputedStyle(fileBox).visibility;
+const img = document.querySelector('#img');
 
-// console.log(changeImg);
-function load(input) {
-  var file = input.file[0];
-  imgBox.src= URL.createObjectURL(file);
-}
+document.querySelector('input').addEventListener('change', function(){
+  console.log('??')
+  const reader = new FileReader();
+})
 
 fileBox.style.visibility = 'hidden';
 fileBoxVisible = 'hidden';
 // console.log(fileBoxVisible);
 
-
 changeImg.addEventListener('click', event=>{
-  if(fileBoxVisible === 'visible'){
-    fileBox.style.visibility = 'hidden';
-    fileBoxVisible = 'hidden';
-    console.log(fileBoxVisible);
-  }
-  else if(fileBoxVisible === 'visible'){
-    fileBox.style.visibility = 'hidden';
-    fileBoxVisible = 'hidden';
-    console.log(fileBoxVisible);
-  }
+  console.log('?');
+  fileBox.style.visibility = 'visible';
+  fileBoxVisible = 'visible';
+  console.log(fileBoxVisible);
 })
 
-
+fileBox.addEventListener('click',e =>{
+  fileBox.style.visibility = 'hidden';
+  fileBoxVisible = 'hidden';
+});
